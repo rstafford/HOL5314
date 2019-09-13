8b9925863387a117d79602ada691fb7e1f5e4e2b
Tim Middleton <tim.middleton@oracle.com>
2019-09-13 12:58:24 +0800

    add port forward script

---
+++ bin/port-forward-kibana.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+trap "exit" INT
+
+if [ $# -ne 1 ] ; then
+   echo "Usage: $0 [namespace]"   
+   fi
+fi
+namespace=$1
+   
+export KIBANA_POD=$(kubectl get pods --namespace $namespace | grep kibana | awk '{print $1}')
+echo "Port-forwarding $KIBANA_POD in $namespace"
+
+while :
+do
+   kubectl port-forward --namespace $namespace $KIBANA_POD 5601:5601
+done
